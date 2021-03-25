49da7295d0116531e72bb2b0692693f3821654fe
240428037 <240428037@qq.com>
2021-03-25 15:11:45 +0800

    提交代码

---
+++ 20210325/20210325/test.c
@@ -0,0 +1,164 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+
+//void init(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		arr[i] = 0;
+//	}
+//}
+//void print(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//void reverse(int arr[], int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left < right)
+//	{
+//		int tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//int main()
+//{
+//	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//init(arr, sz);
+//	print(arr, sz);
+//	reverse(arr, sz);
+//	print(arr, sz);
+//	system("pause");
+//	return 0;
+//}
+//
+//
+//int main()
+//{
+//	system("pause");
+//	return 0;
+//}
+
+
+
+//int get_diff_bit(int m, int n)
+//{
+//	int tmp = m^n;
+//	int count = 0;
+//	while (tmp)
+//	{
+//		tmp = tmp&(tmp - 1);
+//		count++;
+//	}
+//}
+//int main()
+//{
+//	int m = 0;
+//	int n = 0;
+//	scanf("%d%d", &m, &n);
+//	int count = get_diff_bit(m, n);
+//	printf("count=%d\n", count);
+//	return 0;
+//}
+
+
+//int count_bit_one(int n)
+//{
+//      int count = 0;
+//      while (n)
+//      {
+//             if (n%2 == 1)
+//             {
+//                     count++;
+//             }
+//             n = n / 2;
+//      }
+//      return count;
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int count = count_bit_one(a);
+//	printf("count=%d\n", count);
+//	return 0;
+//}
+
+
+
+//int count_bit_one(unsigned int n)
+//{
+//      int count = 0;
+//      while (n)
+//      {
+//             if (n%2 == 1)
+//             {
+//                     count++;
+//             }
+//             n = n / 2;
+//      }
+//      return count;
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int count = count_bit_one(a);
+//	printf("count=%d\n", count);
+//	return 0;
+//}
+
+
+//int count_bit_one(int n)
+//{
+//      int count = 0;
+//      int i = 0;
+//      for (i = 0; i < 32;i++)
+//      {
+//             if (((n >> 1) & 1) == 1)
+//             {
+//                     count++;
+//             }             
+//      }
+//      return count;
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int count = count_bit_one(a);
+//	printf("count=%d\n", count);
+//	return 0;
+//}
+
+//int count_bit_one(int n)
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		n = n&(n - 1);
+//		count++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int count = count_bit_one(a);
+//	printf("count=%d\n", count);
+//	return 0;
+//}
